19d6059804007aea3cc584e2d8f6751b37a9f669
Ricardo Hui <eehaha@gmail.com>
2019-06-01 15:00:48 +0800

    add chair and gesture recognizer

--- FurnitureApp/ViewController.swift
+++ FurnitureApp/ViewController.swift
@@ -13,10 +13,15 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    private var hud: MBProgressHUD!
+    private var newAngleY: Float = 0.0
+    private var currentAngleY: Float = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.sceneView.autoenablesDefaultLighting = true
+        self.hud = MBProgressHUD.showAdded(to: self.sceneView, animated: true)
+        self.hud.label.text = "Detecting Plane..."
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -24,10 +29,97 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        registerGestureRecognizers()
+    }
+    
+    private func registerGestureRecognizers(){
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        let pinchGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(pinched))
+        self.sceneView.addGestureRecognizer(pinchGestureRecognizer)
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panned))
+        self.sceneView.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    @objc func panned(recognizer: UIPanGestureRecognizer){
+        if recognizer.state == .changed {
+            
+            guard let sceneView = recognizer.view as? ARSCNView else {
+                return
+            }
+            
+            let touch = recognizer.location(in: sceneView)
+            let translation = recognizer.translation(in: sceneView)
+            
+            let hitTestResults = self.sceneView.hitTest(touch, options: nil)
+            
+            if let hitTest = hitTestResults.first {
+                
+                let chairNode = hitTest.node
+                
+                self.newAngleY = Float(translation.x) * (Float) (Double.pi)/180
+                self.newAngleY += self.currentAngleY
+                chairNode.eulerAngles.y = self.newAngleY
+                
+                
+            }
+            
+        }
+        else if recognizer.state == .ended {
+            self.currentAngleY = self.newAngleY
+        }
+    }
+    
+    @objc func pinched(recognizer: UIPinchGestureRecognizer){
+        if recognizer.state == .changed{
+            guard let sceneView  = recognizer.view as? ARSCNView else{
+                return
+            }
+            let touch = recognizer.location(in: sceneView)
+            let hitTestResults = self.sceneView.hitTest(touch, options: nil)
+            if let hitTest  = hitTestResults.first{
+                let chairNode = hitTest.node
+                let pinchScaleX = Float(recognizer.scale) * chairNode.scale.x
+                let pinchScaleY = Float(recognizer.scale) * chairNode.scale.y
+                let pinchScaleZ = Float(recognizer.scale) * chairNode.scale.z
+                chairNode.scale = SCNVector3(pinchScaleX, pinchScaleY, pinchScaleZ)
+                recognizer.scale = 1
+            }
+        }
+        
+    }
+    
+    @objc func tapped(recognizer: UITapGestureRecognizer){
+        guard let sceneView = recognizer.view as? ARSCNView else {
+            return
+        }
+        let touch = recognizer.location(in: sceneView)
+        let hitTestResults  = sceneView.hitTest(touch, types: .existingPlane)
+        if let hitTest  = hitTestResults.first{
+            
+            let chairScene = SCNScene(named: "chair.dae")!
+            
+            guard let chairNode = chairScene.rootNode.childNode(withName: "chair", recursively: true) else {
+                return
+            }
+            
+            chairNode.position  = SCNVector3(hitTest.worldTransform.columns.3.x, hitTest.worldTransform.columns.3.y, hitTest.worldTransform.columns.3.z)
+            self.sceneView.scene.rootNode.addChildNode(chairNode)
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor  is ARPlaneAnchor{
+            DispatchQueue.main.async{
+                self.hud.label.text = "Plane Detected"
+                self.hud.hide(animated: true, afterDelay: 1.0)
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +127,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -47,29 +140,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+
 }
